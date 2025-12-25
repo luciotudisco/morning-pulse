b9cb547c127f36ef460a6b65f653564e453e1591
luciotudisco <lucio.tudisco@gmail.com>
2025-12-25 11:23:31 +0100

    Update

--- workflows/tasks.py
+++ workflows/tasks.py
@@ -15,7 +15,7 @@ app.conf.update(
     beat_schedule={
         "hello-world-every-5-minutes": {
             "task": "tasks.hello_world",
-            "schedule": 5.0,  # Run every 5 minutes (300 seconds)
+            "schedule": 300.0,  # Run every 5 minutes (300 seconds)
         },
     },
 )
